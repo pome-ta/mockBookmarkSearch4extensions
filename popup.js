ae984be5e2fc2d3c0594822c5f0ee4c4c87ee2ff
pome-ta <pometarou93@gmail.com>
2022-03-26 12:38:41 +0900

    up

--- popup.js
+++ popup.js
@@ -36,7 +36,10 @@ function getBookmarksList(bookmarkTreeNodes) {
 
 
 const json_path = './dummyData/bookmark.js';
-// const uri = get_path(json_path);
+
+const container = document.querySelector('#container');
+
+
 
 document.addEventListener('DOMContentLoaded', async () => {
     console.log('DOM');
@@ -45,4 +48,64 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.log(all_bookmark_tree);
     const bookmarksList = getBookmarksList(all_bookmark_tree);
     console.log(bookmarksList);
+    container.innerHTML = setBookmarksTableHTML(bookmarksList);
+
+
 });
+
+
+function setBookmarksTableHTML(bookmarks) {
+    // let insertHtml = '<input type="search" class="light-table-filter" data-table="order-table" placeholder="ブックマーク検索" /><table class="order-table"><thead>';
+    let insertHtml = '<input type="search" class="light-table-filter" data-table="order-table" placeholder="ブックマーク検索"  autofocus /><table class="order-table">';
+    // insertHtml += '<tr><th>favicon</th><th>title</th><th>URL</th></tr></thead><tbody>';
+    insertHtml += '<tbody>';
+    for (const bm of bookmarks) {
+        insertHtml += `
+        <tr>
+            <td><img src="${bm.favicon}"></td>
+            <td><div><a href="${bm.url}">${bm.title}</a>${bm.url}</div></td>
+        </tr>`;
+    }
+    insertHtml += '</tbody></table>';
+    return insertHtml;
+
+}
+
+// http://kachibito.net/snippets/light-javascript-table-filter
+const LightTableFilter = (Arr => {
+    let _input;
+    function _onInputEvent(e) {
+        _input = e.target;
+        let tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+        console.log(tables);
+        Arr.forEach.call(tables, table => {
+            Arr.forEach.call(table.tBodies, tbody => {
+                Arr.forEach.call(tbody.rows, _filter);
+            });
+        });
+    }
+    function _filter(row) {
+        console.log(row);
+        let text = row.textContent.toLowerCase();
+        let val = _input.value.toLowerCase();
+        row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
+    }
+    return {
+        init: function () {
+            let inputs = document.getElementsByClassName('light-table-filter');
+            Arr.forEach.call(inputs, input => {
+                console.log(input);
+                input.oninput = _onInputEvent;
+            });
+        }
+    };
+})(Array.prototype);
+
+document.addEventListener('readystatechange', function () {
+    console.log('hoge');
+    if (document.readyState === 'complete') {
+        console.log('load');
+        LightTableFilter.init();
+    }
+});
+
